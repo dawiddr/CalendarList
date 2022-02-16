57c17f5deb5f03307c992a5cf5c2f0161076c875
Dawid Drechny <dawiddr@gmail.com>
2022-02-16 21:32:33 +0100

    Fix a compile error

--- Sources/CalendarList/Calendar views/CalendarMonthView.swift
+++ Sources/CalendarList/Calendar views/CalendarMonthView.swift
@@ -27,44 +27,63 @@ struct CalendarMonthView<T:Hashable>: View {
             VStack(spacing: 0) {
                 ForEach(self.month.weeks, id:\.self) { week in
                     HStack(spacing: 0) {
-                        if week.count < 7 && self.containsFirstDayOfMonth(week) {
-                            ForEach(1 ... 7 - week.count, id:\.self) { num in
-                                Text("")
-                                    .frame(width: self.dayViewWidth(parentWidth: geometry.size.width),
-                                           height: self.calendarDayHeight)
-                                    
-                            }
+                        ForEach(getRangeForMarginsTop(week: week), id:\.self) { num in
+                            Text("")
+                                .frame(
+                                    width: self.dayViewWidth(parentWidth: geometry.size.width),
+                                    height: self.calendarDayHeight
+                            )
                         }
 
                         ForEach(week, id:\.self) { day in
-                            CalendarViewDay(calendar: self.calendar,
-                                            day: day,
-                                            selected: CalendarUtils.isSameDay(date1:self.selectedDate, date2:day, calendar: self.calendar),
-                                            hasEvents: self.dayHasEvents(day),
-                                            width: self.dayViewWidth(parentWidth: geometry.size.width),
-                                            height: self.calendarDayHeight,
-                                            selectedDateColor: self.selectedDateColor,
-                                            todayDateColor: self.todayDateColor)
-                                .onTapGesture {
-                                    self.selectedDate = day
-                                }
-                        }
-
-                        if week.count < 7 && !self.containsFirstDayOfMonth(week) {
-                            ForEach(1 ... 7 - week.count, id:\.self) { num in
-                                Text("")
-                                .frame(width: self.dayViewWidth(parentWidth: geometry.size.width),
-                                       height: self.calendarDayHeight)
+                            CalendarViewDay(
+                                calendar: self.calendar,
+                                day: day,
+                                selected: CalendarUtils.isSameDay(
+                                    date1: self.selectedDate,
+                                    date2: day,
+                                    calendar: self.calendar
+                                ),
+                                hasEvents: self.dayHasEvents(day),
+                                width: self.dayViewWidth(parentWidth: geometry.size.width),
+                                height: self.calendarDayHeight,
+                                selectedDateColor: self.selectedDateColor,
+                                todayDateColor: self.todayDateColor
+                            )
+                            .onTapGesture {
+                                self.selectedDate = day
                             }
                         }
-                    }
                         
+                        ForEach(getRangeForMarginsBottom(week: week), id:\.self) { num in
+                            Text("")
+                                .frame(
+                                    width: self.dayViewWidth(parentWidth: geometry.size.width),
+                                    height: self.calendarDayHeight
+                                )
+                        }
+                    }
                     .padding([.leading, .trailing], 10)
                 }
             }
         }
     }
-        
+
+    func getRangeForMarginsTop(week: [Date]) -> Range<Int> {
+        if week.count < 7 && self.containsFirstDayOfMonth(week) {
+            let diff = 7 - week.count
+            return 1..<diff+1
+        }
+        return 0..<0
+    }
+
+    func getRangeForMarginsBottom(week: [Date]) -> Range<Int> {
+        if week.count < 7 && !self.containsFirstDayOfMonth(week) {
+            let diff = 7 - week.count
+            return 1..<diff+1
+        }
+        return 0..<0
+    }
     func dayViewWidth(parentWidth:CGFloat) -> CGFloat {
         return (parentWidth - 20) / 7
     }
